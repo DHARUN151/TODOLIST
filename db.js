fa473c8c59d2885f362992b372f85caaf784dcb8
DHARUN KUMAR <83487143+DHARUN151@users.noreply.github.com>
2025-09-13 09:13:54 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 async function connectDB(){
-    const connection = await mongoose.connect("mongodb+srv://dharuns2006:OaMYql94va91H8nB@cluster0.jdjg6qw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    const connection = await mongoose.connect("")
     console.log("Database connected successfully");
 } 
+
 
 module.exports = connectDB;
